af590958cb16ca6062785294803dd51d75a9ba73
jordankmaze <jordankmaze@gmail.com>
2017-08-02 19:17:08 -0700

    Updates
    updates to style and functionality

--- public/Date/date-view/dateView.component.js
+++ public/Date/date-view/dateView.component.js
@@ -9,14 +9,17 @@
         vm.id = '1';
         vm.lastNumber = 0;
 
-        vm.randomNumber = function () {
-            for (var key in vm.dates) {
-                if (vm.dates[key].category == vm.categorySelected.category) {
-                    vm.searchDateArray.push(vm.dates[key]);
-                }
-            }
+        vm.changedValue = function () {
+          vm.searchDateArray = [];
+          for (var key in vm.dates) {
+              if (vm.dates[key].category == vm.categorySelected.category) {
+                  vm.searchDateArray.push(vm.dates[key]);
+              }
+          }
+        }
 
-            var getRandomNumber = Math.floor(Math.random() * key) + 0;
+        vm.randomNumber = function () {
+            var getRandomNumber = Math.floor(Math.random() * vm.searchDateArray.length) + 0;
 
             if(getRandomNumber != vm.lastNumber){
                 vm.rand = vm.searchDateArray[getRandomNumber].desc;
@@ -35,31 +38,55 @@
              "index": 0,
              "desc": "Have a picnic in Laurelhurst Park",
              "price": 0,
-             "category": "fun"
+             "category": "<$10"
          },
          {
              "index": 1,
              "desc": "See a movie outside",
              "price": 0,
-             "category": "fun"
+             "category": "<$10"
          },
          {
              "index": 2,
              "desc": "Get cozy at Living Room Theater",
              "price": 5,
-             "category": "fun"
+             "category": "<$10"
          },
          {
              "index": 3,
              "desc": "See free art at the Portland Art Museum",
              "price": 5,
-             "category": "nofun"
+             "category": "<$10"
          },
          {
              "index": 4,
              "desc": "Play mini golf at Oaks Park",
              "price": 6,
-             "category": "nofun"
+             "category": "<$10"
+         },
+         {
+             "index": 5,
+             "desc": "Tigard Balloon Festival",
+             "price": 30,
+             "category": ">$10"
+         },
+         {
+             "index": 6,
+             "desc": "Stand in line at Salt and Straw",
+             "price": 12,
+             "category": ">$10"
+         },
+         {
+             "index": 7,
+             "desc": "Dinner at 23Hoyt",
+             "price": 50,
+             "category": ">$10"
+         },
+         {
+             "index": 8,
+             "desc": "Drinks at Sauce ",
+             "price": 15,
+             "category": ">$10"
          }
         ]
     }];
